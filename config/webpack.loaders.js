e18453d59c5acb2743a811c2dbb0e2244a1773b3
ashleykolodziej <kolodz10@bu.edu>
2021-11-11 10:46:14 -0500

    Fix an issue where sometimes images will not load

--- config/webpack.loaders.js
+++ config/webpack.loaders.js
@@ -7,139 +7,135 @@ const sourceMap = config.env !== 'production';
 
 // HTML loaders
 const html = {
-  test: /\.(html)$/,
-  use: [
-    {
-      loader: 'html-loader',
-    },
-  ],
+	test: /\.(html)$/,
+	use: [
+		{
+			loader: 'html-loader',
+		},
+	],
 };
 
 // Javascript loaders
 const js = {
-  test: /\.js(x)?$/,
-  exclude: /node_modules/,
-  use: [
-    {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-      },
-    },
-    'eslint-loader',
-  ],
+	test: /\.js(x)?$/,
+	exclude: /node_modules/,
+	use: [
+		{
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+			},
+		},
+		'eslint-loader',
+	],
 };
 
 // Style loaders
 const styleLoader = {
-  loader: 'style-loader'
+	loader: 'style-loader'
 };
 
 const cssLoader = {
-  loader: 'css-loader',
-  options: {
-    sourceMap,
-  },
+	loader: 'css-loader',
+	options: {
+		sourceMap,
+	},
 };
 
 const postcssLoader = {
-  loader: 'postcss-loader',
-  options: {
+	loader: 'postcss-loader',
+	options: {
 		postcssOptions: {
-			plugins: [ autoprefixer ]
+			plugins: [autoprefixer]
 		}
-  },
+	},
 };
 
 const css = {
-  test: /\.css$/,
-  use: [
-    config.env === 'production' ? MiniCssExtractPlugin.loader : styleLoader,
-    cssLoader,
-    postcssLoader,
-  ],
+	test: /\.css$/,
+	use: [
+		config.env === 'production' ? MiniCssExtractPlugin.loader : styleLoader,
+		cssLoader,
+		postcssLoader,
+	],
 };
 
 const sass = {
-  test: /\.s[c|a]ss$/,
-  use: [
-    config.env === 'production' ? MiniCssExtractPlugin.loader : styleLoader,
-    cssLoader,
-    postcssLoader,
-    {
-      loader: 'sass-loader',
-      options: {
-        sourceMap,
-      },
-    },
-  ],
+	test: /\.s[c|a]ss$/,
+	use: [
+		config.env === 'production' ? MiniCssExtractPlugin.loader : styleLoader,
+		cssLoader,
+		postcssLoader,
+		{
+			loader: 'sass-loader',
+			options: {
+				sourceMap,
+			},
+		},
+	],
 };
 
 // Image loaders
 const imageLoader = {
-  loader: 'image-webpack-loader',
-  options: {
-    bypassOnDebug: true,
-    gifsicle: {
-      interlaced: false,
-    },
-    optipng: {
-      optimizationLevel: 7,
-    },
-    pngquant: {
-      quality: '65-90',
-      speed: 4,
-    },
-    mozjpeg: {
-      progressive: true,
-    },
-  },
+	loader: 'image-webpack-loader',
+	options: {
+		bypassOnDebug: true,
+		gifsicle: {
+			interlaced: false,
+		},
+		optipng: {
+			optimizationLevel: 7,
+		},
+		pngquant: {
+			quality: '65-90',
+			speed: 4,
+		},
+		mozjpeg: {
+			progressive: true,
+		},
+	},
 };
 
 const images = {
-  test: /\.(gif|png|jpe?g|svg)$/i,
-  exclude: /fonts/,
-  use: [
-    'file-loader?name=images/[name].[hash].[ext]',
-    config.env === 'production' ? imageLoader : null,
-  ].filter(Boolean),
+	test: /\.(gif|png|jpe?g|svg)$/i,
+	type: 'asset/resource'
 };
 
 // Font loaders
 const fonts = {
-  test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-  exclude: /images/,
-  use: [
-    {
-      loader: 'file-loader',
-      options: {
-        name: '[name].[hash].[ext]',
-        outputPath: 'fonts/',
-      },
-    },
-  ],
+	test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+	exclude: /images/,
+	use: [
+		{
+			loader: 'file-loader',
+			options: {
+				name: '[name].[hash].[ext]',
+				outputPath: 'fonts/',
+			},
+		},
+	],
 };
 
 // Video loaders
 const videos = {
-  test: /\.(mp4|webm)$/,
-  use: [
-    {
-      loader: 'file-loader',
-      options: {
-        name: '[name].[hash].[ext]',
-        outputPath: 'images/',
-      },
-    },
-  ],
+	test: /\.(mp4|webm)$/,
+	use: [
+		{
+			loader: 'file-loader',
+			options: {
+				name: '[name].[hash].[ext]',
+				outputPath: 'images/',
+			},
+		},
+	],
 };
 
 module.exports = [
-  html,
-  js,
-  css,
-  sass,
-  images,
-  fonts,
-  videos,
+	html,
+	js,
+	css,
+	sass,
+	images,
+	fonts,
+	videos,
 ];

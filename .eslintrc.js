23b8d922c7ff3d1f079cd949c9078307579f35b8
Ashley Kolodziej <kolodz10@bu.edu>
2021-12-04 16:03:36 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,10 @@ module.exports = {
 	"extends": "airbnb-base",
 	"rules": {
 		"linebreak-style": 0,
+		"no-tabs": 0,
+		"indent": [2, "tab"],
+		"guard-for-in": 0,
+		"no-restricted-syntax": 0,
 	},
 	"env": {
 		"browser": true,

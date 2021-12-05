0ca588aa143d4c63faa1f96ac32674ef9feed6d7
sym971023 <90291361+sym971023@users.noreply.github.com>
2021-12-04 22:09:58 -0500

    Final 2n check 1.2

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,26 +1,50 @@
 const body = document.querySelector('#starshine');
-		const template = document.querySelector('.template.shine');
-		const stars = 20;
-		const sparkle = 20;
-		let size = 'small';
-		const createStar = function () {
-			const star = template.cloneNode(true);
-			star.style.top = `${Math.random() * 100}%`;
-			star.style.left = `${Math.random() * 100}%`;
-			star.animationDelay = `${Math.random() * sparkle}s`;
-			star.classList.add(size);
-			body.insertAdjacentElement('beforeend', star);
-		};
+const template = document.querySelector('.template.shine');
+const stars = 20;
+const sparkle = 20;
 
-		// Creates as many stars as your variable above says to, randomly.
-		for (var i = 0; i < stars; i++) {
-			if (i % 2 === 0) {
-				size = 'small';
-			} else if (i % 3 === 0) {
-				size = 'medium';
-			} else {
-				size = 'large';
-			}
+let size = 'small';
+function createStar() {
+	const star = template.cloneNode(true);
+	star.style.top = `${Math.random() * 100}%`;
+	star.style.left = `${Math.random() * 100}%`;
+	star.animationDelay = `${Math.random() * sparkle}s`;
+	star.classList.add(size);
+	body.insertAdjacentElement('beforeend', star);
+}
 
-			createStar();
-		}
+// Creates as many stars as your variable above says to, randomly.
+for (let i = 0; i < stars; i++) {
+	if (i % 2 === 0) {
+		size = 'small';
+	} else if (i % 3 === 0) {
+		size = 'medium';
+	} else {
+		size = 'large';
+	}
+
+	createStar();
+}
+/*
+function createStar() {
+	const star = template.cloneNode(true);
+	star.style.top = `${Math.random() * 100}%`;
+	star.style.left = `${Math.random() * 100}%`;
+	star.animationDelay = `${Math.random() * sparkle}s`;
+	star.classList.add(size);
+	body.insertAdjacentElement('beforeend', star);
+}
+
+// Creates as many stars as your variable above says to, randomly.
+for (var i = 0; i < stars; i++) {
+	if (i % 2 === 0) {
+		size = 'small';
+	} else if (i % 3 === 0) {
+		size = 'medium';
+	} else {
+		size = 'large';
+	}
+
+	createStar();
+}
+*/
